8a5227647d912b695faa76c498bcff471c08c4c1
Abhishek Jain <abhishek.jain@tallysolutions.com>
2015-08-09 00:57:16 +0530

    Added windows compilation

---
+++ template/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// g++ -std=c++11 -Wall -O3 -m64 -g3 -o main.exe
+// g++ -std=c++11 -Wall -O0 -m64 -g3 -o main.exe -DDEBUG
+// cl -Ox /Z7 /W3 /DEBUG /Fo.\bin\win_release\ main.cpp /link /out:bin\win_release\main.exe
+// cl -Od /Z7 /W3 /DDEBUG /DEBUG /Fo.\bin\win_debug\ main.cpp /link /out:bin\win_debug\main.exe
+
+int main ()
+{
+    return 0;
+}

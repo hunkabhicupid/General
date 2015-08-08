ecfc493a593c0ea252b354147a74033449825a31
abhishek.jain <abhishek.jain@tallysolutions.com>
2015-08-09 01:07:57 +0530

    Linux compilation

--- template/main.cpp
+++ template/main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-// g++ -std=c++11 -Wall -O3 -m64 -g3 -o main.exe
-// g++ -std=c++11 -Wall -O0 -m64 -g3 -o main.exe -DDEBUG
+// g++ -std=c++11 -Wall -O3 -m64 -g3 -o ./bin/lin_release/main main.cpp
+// g++ -std=c++11 -Wall -O0 -m64 -g3 -o ./bin/lin_debug/main -DDEBUG main.cpp
 // cl -Ox /Z7 /W3 /DEBUG /Fo.\bin\win_release\ main.cpp /link /out:bin\win_release\main.exe
 // cl -Od /Z7 /W3 /DDEBUG /DEBUG /Fo.\bin\win_debug\ main.cpp /link /out:bin\win_debug\main.exe
 
 int main ()
 {
+    printf("\nHello!!\n");
     return 0;
 }
